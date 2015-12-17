f417b5f6d6dece158bc29fa200d8a664ff55580f
jshulk <vijaytyagi.vt@gmail.com>
2015-12-17 20:51:29 +0530

    mails working now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,15 +17,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: "me@gmail.com",
-    password: "my_password"
-    authentication: :login,
-    enable_starttls_auto: false
+    address: ENV["SMTP_SERVER"],
+    port: "587",
+    user_name: ENV["SMTP_USER"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: ENV["SMTP_DOMAIN"],
+    authentication: :plain
   }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
